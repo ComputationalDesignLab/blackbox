303e3afada15440042b0a6f395804c2b950797fb
pkck28 <kcpkumar33@gmail.com>
2025-08-10 19:37:15 -0400

    updated dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='blackbox',
-    version='1.0.0',
-    packages=find_packages(include=['blackbox*']),
-    install_requires=["numpy>=1.21.6", 
-                      "scipy>=1.7.3", 
-                      "mpi4py>=3.1.3", 
-                      "pygeo>=1.12.2", 
-                      "prefoil>=2.0.0", 
-                      "smt>=2.0.0", 
-                      "psutil>=5.9.1"],
+    name="blackbox",
+    version="1.0.0",
+    packages=find_packages(include=["blackbox*"]),
+    python_requires=">=3.11",
+    install_requires=["numpy>=1.26.0", 
+                      "scipy>=1.11.1",
+                      "pyDOE3>=1.1.0"
+                    ],
 )
