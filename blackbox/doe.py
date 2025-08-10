35e6f9197eaa73924be9496d12722b13f7be6186
pkck28 <kcpkumar33@gmail.com>
2025-08-10 19:39:42 -0400

    added lhs sampling method

---
+++ blackbox/doe.py
@@ -0,0 +1,20 @@
+# This module defines various function for generating sampling plan using different methods
+
+from pyDOE3 import lhs
+from scipy.stats.qmc import Sobol, Halton
+
+def generate_lhs_samples(problem, n_samples, criterion="cm", iterations=100, random_state=None):
+
+    samples = lhs(problem.dim, n_samples, criterion, iterations, random_state)
+
+    samples = problem.lb + (problem.ub - problem.lb) * samples
+
+    return samples
+
+def generate_sobol_samples(problem, n_samples, scramble=False, random_state=None):
+
+    pass
+
+def generate_halton_samples(problem, n_samples, scramble=False, random_state=None):
+
+    pass
