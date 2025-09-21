b1f993615248f5112078f0b6fe3b6eff0f1bc50f
pkck28 <kcpkumar33@gmail.com>
2025-09-21 19:08:32 -0400

    updated mpi version and package version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='blackbox',
-    version='1.0.0',
+    version='0.9.0',
     packages=find_packages(include=['blackbox*']),
     install_requires=["numpy>=1.21.6", 
                       "scipy>=1.7.3", 
-                      "mpi4py>=3.1.3", 
+                      "mpi4py>=3.1.4", 
                       "pygeo>=1.12.2", 
                       "prefoil>=2.0.0", 
                       "smt>=2.0.0", 
