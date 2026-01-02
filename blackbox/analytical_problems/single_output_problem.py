dc33d00d9745647a340b7d85e0edce421a6761ea
pkck28 <kcpkumar33@gmail.com>
2026-01-02 18:33:21 -0500

    updated function handles and added modified branin function

--- blackbox/analytical_problems/single_output_problem.py
+++ blackbox/analytical_problems/single_output_problem.py
@@ -1,105 +1,9 @@
 import numpy as np
-from ..msg import print_msg
-from abc import abstractmethod
-
-class SingleOutputAnalyticalProblem():
-
-    def __init__(self, num_inputs: int, negate: bool):
-        """
-            Base class for all single output analytical problems
-        """
-
-        try:
-            assert isinstance(num_inputs, int) and num_inputs > 0, "num_inputs must be a positive integer"
-            assert isinstance(negate, bool), "negate must be a boolean variable"
-        
-        except AssertionError as e:
-            print_msg(str(e))
-
-        self.num_inputs = num_inputs
-        self.num_outputs = 1
-        self.negate = negate
-
-    def __call__(self, x: np.ndarray) -> np.ndarray:
-        """
-            Evalaute the function for given input x
-
-            Parameters
-            ----------
-            x: np.ndarray
-                1D/2D numpy array of shape (dim,) or (n_samples,dim)
-
-            Returns
-            -------
-            y: np.ndarray
-                1D/2D numpy array of shape (n_samples,1) or (1,) 
-                containing the required output value for each input sample
-        """
-
-        self._check_input(x)
-
-        y = self._evaluate(np.atleast_2d(x)) # evaluate the function
-        
-        y = y.reshape(-1,self.num_outputs) # ensure output is 2D
-
-        if self.negate:
-            y = -y
-
-        if x.ndim == 1:
-            y = y.reshape(-1,)
-        
-        return y
-    
-    def set_bounds(self, bounds: np.ndarray):
-        """
-            Method to set the bounds or change the default bounds of the problem
-
-            Parameter
-            ---------
-            bounds: np.ndarray
-                2D numpy array of shape (2,num_inputs) containing the new lower and upper bounds.
-                First and second row correspond to lower and upper bounds, respectively
-        """
-
-        try:
-            assert isinstance(bounds, np.ndarray), "bounds must be a numpy array"
-            assert bounds.ndim == 2, "bounds must be a 2D numpy array"
-            assert bounds.shape == (2,self.num_inputs), f"bounds must have shape (2,{self.num_inputs})"
-            assert np.all(bounds[0,:] < bounds[1,:]), "Lower bounds must be less than upper bounds"
-        
-        except AssertionError as e:
-            print_msg(str(e))
-
-        self.lb = bounds[0,:]
-        self.ub = bounds[1,:]
-
-    def _check_input(self, x: np.ndarray):
-        """
-            Method to validate the input before evaluation
-
-            Parameters
-            ----------
-            x: np.ndarray
-                Input sample(s) to be evaluated, can be either a 1D or 2D numpy array
-        """
-
-        x = np.atleast_2d(x)
-
-        try:
-            assert x.shape[1] == self.lb.shape[0], "Input dimension must match the problem dimension"
-            assert np.all(x >= self.lb) and np.all(x <= self.ub), "Input values must be within the bounds defined by lb and ub"
-            
-        except AssertionError as e:
-            print_msg(str(e))
-    
-    @abstractmethod
-    def _evaluate(self, x: np.ndarray) -> np.ndarray:
-        pass
-
+from ..base_classes.analytical_base_problem import SingleOutputAnalyticalProblem
 
 class Ackley(SingleOutputAnalyticalProblem):
 
-    def __init__(self, num_inputs: int = 10, negate=False):
+    def __init__(self, num_inputs: int = 10, negate: bool = False):
         """
             Class for defining the ackley function
 
@@ -130,7 +34,7 @@ class Ackley(SingleOutputAnalyticalProblem):
 
 class Levy(SingleOutputAnalyticalProblem):
 
-    def __init__(self, num_inputs: int = 10, negate=False):
+    def __init__(self, num_inputs: int = 10, negate: bool = False):
         """
             Class for defining the levy function
 
@@ -162,7 +66,7 @@ class Levy(SingleOutputAnalyticalProblem):
 
 class Rastrigin(SingleOutputAnalyticalProblem):
 
-    def __init__(self, num_inputs: int = 10, negate=False):
+    def __init__(self, num_inputs: int = 10, negate: bool = False):
         """
             Class for defining the rastrigin function
 
@@ -174,7 +78,7 @@ class Rastrigin(SingleOutputAnalyticalProblem):
                 negate the values before returning
         """
 
-        super().__init__(num_inputs,negate)
+        super().__init__(num_inputs, negate)
 
         self.lb = np.array([-5.12]*num_inputs)
         self.ub = np.array([5.12]*num_inputs)
@@ -203,11 +107,7 @@ class Hartmann(SingleOutputAnalyticalProblem):
         self.lb = np.zeros(self.num_inputs)
         self.ub = np.ones(self.num_inputs)
 
-        try:
-            assert self.num_inputs in [3,4,6], "Hartmann function is only defined for 3, 4 or 6 dimensions"
-        
-        except AssertionError as e:
-            print_msg(str(e))
+        assert self.num_inputs in [3,4,6], "Hartmann function is only defined for 3, 4 or 6 dimensions"
 
         if self.num_inputs == 3:
 
@@ -258,7 +158,7 @@ class Hartmann(SingleOutputAnalyticalProblem):
 
 class Branin(SingleOutputAnalyticalProblem):
 
-    def __init__(self, negate=False):
+    def __init__(self, negate: bool = False):
         """
             Class for defining the branin function
 
@@ -286,3 +186,35 @@ class Branin(SingleOutputAnalyticalProblem):
         x2 = x[:,1]
 
         return a * (x2 - b * x1**2 + c * x1 - r)**2 + s * (1 - t) * np.cos(x1) + s
+
+
+class ModifiedBranin(SingleOutputAnalyticalProblem):
+
+    def __init__(self, negate: bool = False):
+        """
+            Class for defining the modified branin function
+
+            Parameters
+            ----------
+            negate: bool
+                negate the values before returning
+        """
+
+        super().__init__(num_inputs=2, negate=negate)
+
+        self.lb = np.array([-5.0, 0.0])
+        self.ub = np.array([10.0, 15.0])
+    
+    def _evaluate(self, x:np.ndarray) -> np.ndarray:
+
+        a = 1.0
+        b = 5.1 / (4.0 * np.pi**2)
+        c = 5.0 / np.pi
+        r = 6.0
+        s = 10.0
+        t = 1.0 / (8.0 * np.pi)
+
+        x1 = x[:,0]
+        x2 = x[:,1]
+
+        return a * (x2 - b * x1**2 + c * x1 - r)**2 + s * (1 - t) * np.cos(x1) + s + 5*x1
