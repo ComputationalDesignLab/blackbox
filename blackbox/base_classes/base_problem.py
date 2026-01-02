7176939f327dea18632f540cc6829bcafbfa012a
Pavankumar Koratikere <kcpkumar33@gmail.com>
2026-01-02 12:34:33 -0500

    updated base problem class to include only abstract call method

--- blackbox/base_classes/base_problem.py
+++ blackbox/base_classes/base_problem.py
@@ -1,6 +1,5 @@
 from abc import abstractmethod, ABC
 import numpy as np
-from ..msg import print_msg
 
 class BaseProblem(ABC):
     """
@@ -10,22 +9,3 @@ class BaseProblem(ABC):
     @abstractmethod
     def __call__(self, x: np.ndarray) -> np.ndarray:
         pass
-
-    def check_input(self, x: np.ndarray):
-        """
-            Method to validate the input before evaluation
-
-            Parameters
-            ----------
-            x: np.ndarray
-                Input sample(s) to be evaluated, can be either a 1D or 2D numpy array
-        """
-
-        x = np.atleast_2d(x)
-
-        try:
-            assert x.shape[1] == self.in_dim, "Input dimension must match the problem dimension"
-            assert np.all(x >= self.lb) and np.all(x <= self.ub), "Input values must be within the bounds defined by lb and ub"
-            
-        except AssertionError as e:
-            print_msg(str(e))
